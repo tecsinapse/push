76c9be6badcfcbdc29245d4a0b6c59b7225fcc0c
Ben Strahan <ben@benstrahan.com>
2015-12-04 21:41:26 -0600

    Comment out Cordova plugins because build pack errors out

--- package.js
+++ package.js
@@ -14,7 +14,7 @@ Npm.depends({
 
 Cordova.depends({
   // Fix ios 7 and ios in general
-  'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/c4e3aa69c66bde45472e81ac303a9e39020c9cc7'
+  // 'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/c4e3aa69c66bde45472e81ac303a9e39020c9cc7'
   // Fix issue 365 ios 7 missing badge updates
   // 'com.phonegap.plugins.PushPlugin': 'https://github.com/raix/PushPlugin/tarball/ff4ade868488ef0fcb014da652681011cd95d8ea'
   // 'com.clone.phonegap.plugins.pushplugin': '2.4.1' //with #354 fixed OK
